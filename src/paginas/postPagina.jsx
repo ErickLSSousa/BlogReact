3ff936b7477067554141b0cce8f5d2b1eddd109c
vickyprazides <vickyprazides@gmail.com>
2025-06-02 09:17:45 -0300

    Update postPagina.jsx

---
+++ src/paginas/postPagina.jsx
@@ -0,0 +1,11 @@
+import PostDetail from '../components/PostDetail';
+
+function postPagina() {
+    return (
+        <div>
+          <PostDetail />
+        </div>
+    );
+}
+
+export default postPagina;
