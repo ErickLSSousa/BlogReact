94e64799ff0e7136babb32cdaf05288a36df7147
laraggodoy08 <larissagabrielagodoy08@gmail.com>
2025-06-02 11:06:57 -0300

    update api.jsx

--- src/assets/api.jsx
+++ src/assets/api.jsx
@@ -1,15 +1,49 @@
 import axios from 'axios';
 
+
 const api = axios.create({
-  baseURL: 'https://api.example.com'
+  baseURL: 'https://jsonplaceholder.typicode.com',
 });
+
 
 export const buscarUsuarios = async () => {
   try {
-    const resposta = await api.get('/usuarios');
+    const resposta = await api.get('/users'); 
     return resposta.data;
   } catch (error) {
-    console.error(error);
+    console.error('Erro ao buscar usuários:', error);
+    return [];
+  }
+};
+
+
+export const buscarPosts = async () => {
+  try {
+    const resposta = await api.get('/posts');
+    return resposta.data;
+  } catch (error) {
+    console.error('Erro ao buscar posts:', error);
+    return [];
+  }
+};
+
+export const buscarPostPorId = async (id) => {
+  try {
+    const resposta = await api.get(`/posts/${id}`);
+    return resposta.data;
+  } catch (error) {
+    console.error('Erro ao buscar o post:', error);
+    return null;
+  }
+};
+
+export const buscarUsuarioPorId = async (id) => {
+  try {
+    const resposta = await api.get(`/users/${id}`);
+    return resposta.data;
+  } catch (error) {
+    console.error('Erro ao buscar o usuário:', error);
+    return null;
   }
 };
 
