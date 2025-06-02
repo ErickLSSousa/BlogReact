125af8831e102560d9b71b2dcda88deebef3b012
ErickLSSousa <erickrewardd@gmail.com>
2025-06-02 10:12:33 -0300

    arrumei erro post detail

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { fetchPost, fetchUser } from '../api/api';
+import { fetchPost, fetchUser } from '../serviços/api';
 
 function PostPage() {
   const { id } = useParams();
