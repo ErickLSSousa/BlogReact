a1dbc2642b3f033f218c80f8438cf7dfb7e3db8f
vickyprazides <vickyprazides@gmail.com>
2025-06-02 09:08:40 -0300

    Update home.jsx

---
+++ src/paginas/home.jsx
@@ -0,0 +1,12 @@
+import PostList from '../components/PostList';
+
+function Home() {
+    return (
+        <div>
+            <h1>Bem-Vindo ao Blog PostFlix</h1>
+            <PostList />
+        </div>
+    );
+}
+
+export default Home;
